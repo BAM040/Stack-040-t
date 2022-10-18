35abc71434ad3ce708bce64622787e418085186e
Gaber <g.abouzid@hotmail.com>
2022-10-18 09:05:52 +0200

    jahhhhhh

--- resources.tf
+++ resources.tf
@@ -4,3 +4,11 @@ resource "azurerm_resource_group" "lets-go" {
 
   
 }
+
+resource "azurerm_public_ip" "publiekelijk" {
+    name = "dit"
+    resource_group_name = azurerm_resource_group.lets-go
+    location = location.azurerm_resource_group
+    allocation_method = "static"
+  
+}
