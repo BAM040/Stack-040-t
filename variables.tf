e1286abb4c7b3cafb12e200023baf2449d7dfb3a
Gaber <g.abouzid@hotmail.com>
2023-03-08 12:00:33 +0100

    test

---
+++ variables.tf
@@ -0,0 +1,22 @@
+/*variable "resource_group_location" {
+  default     = "west europe"
+  description = "Location of the resource group."
+}*/
+
+variable "prefix" {
+  type        = string
+  default     = "Bam-040"
+  description = "Prefix of the resource name"
+}
+
+variable "admin_username" { 
+  type        = string
+  default     = "Gaber"
+  description = "Prefix of admin username"
+}
+
+variable "admin_password" { 
+  type        = string
+  default     = "Owkeejdann_040"
+  description = "Prefix of admin username"
+}
